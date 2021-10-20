1d007c4f660dcd47fca94a803ada43cd40a4b5e2
Alisson98 <alissonfernandes98@outlook.com>
2021-10-20 12:27:44 -0300

    Validation Forms Implementation

--- src/app/views/components/technician/technician-create/technician-create.component.ts
+++ src/app/views/components/technician/technician-create/technician-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Technician } from 'src/app/models/technician';
 import { TechnicianService } from 'src/app/services/technician.service';
@@ -11,10 +12,15 @@ import { TechnicianService } from 'src/app/services/technician.service';
 export class TechnicianCreateComponent implements OnInit {
   technician: Technician = {
     id: '',
-    name: 'Valdir',
-    cpf: '780.294.680-83',
-    phone: '(99) 90000-0000'
+    name: '',
+    cpf: '',
+    phone: ''
   }
+
+  name = new FormControl('', [Validators.minLength(5)])
+  cpf = new FormControl('', [Validators.minLength(11)])
+  phone = new FormControl('', [Validators.minLength(11)])
+
 
   constructor(
     private router: Router,
@@ -35,6 +41,36 @@ export class TechnicianCreateComponent implements OnInit {
       if (err.error.error.match('CPF is already in use!')) {
         this.service.message(err.error.error)
       }
+      else if (err.error.listErrors[0].message === "número do registro de contribuinte individual brasileiro (CPF) inválido"){
+        this.service.message('Invalid Cpf')
+      }
     })
+  }
+
+  errorValidName() {
+    if(this.name.invalid){
+      return 'The name must be between 5 and 100 characters'
+    }
+    else {
+      return false
+    }
+  }
+
+  errorValidCPF() {
+    if(this.name.invalid){
+      return 'The CPF must be 11 characters'
+    }
+    else {
+      return false
+    }
+  }
+
+  errorValidPhone() {
+    if(this.name.invalid){
+      return 'The CPF must be 11 characters'
+    }
+    else {
+      return false
+    }
   }
 }
